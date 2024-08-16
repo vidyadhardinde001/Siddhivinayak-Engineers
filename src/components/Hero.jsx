d0c97eedf39ec1b121f25168c706551f43fdce41
vidyadhardinde <135449569+vidyadhardinde@users.noreply.github.com>
2024-08-16 16:48:45 +0530

    init

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,25 +22,19 @@ const Hero = () => {
     >
       <div className="container relative" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
-          <h1 className="h1 mb-6">
-            Explore the Possibilities of&nbsp;AI&nbsp;Chatting with {` `}
-            <span className="inline-block relative">
-              Brainwave{" "}
-              <img
-                src={curve}
-                className="absolute top-full left-0 w-full xl:-mt-2"
-                width={624}
-                height={28}
-                alt="Curve"
-              />
-            </span>
+          <h1 className="h1 mb-6 text-5xl">
+            <span className="text-3xl" style={{ color: "#B0B0B0" }}>Welcome to<br /></span>
+            Siddhivinayak Engineers. {` `}
           </h1>
-          <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
-            Unleash the power of AI within Brainwave. Upgrade your productivity
-            with Brainwave, the open AI chat app.
+
+          <p className="body-1 max-w-3xl mx-auto mb-5 text-n-2 lg:mb-8">
+            One Stop Solution for All your Electric & Automation Needs.
           </p>
           <Button href="/pricing" white>
-            Get started
+            Go to projects
+          </Button>
+          <Button href="/pricing" white>
+            Contact Us
           </Button>
         </div>
         <div className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24">
